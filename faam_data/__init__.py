6ed796b6f176af7f571f1e08c8bfd7ccac6c5d78
Dave Sproson <dave.sproson@faam.ac.uk>
2024-02-15 18:36:57 +0000

    Factor out some code to vocal.

--- faam_data/__init__.py
+++ faam_data/__init__.py
@@ -1,50 +1,16 @@
-import os
-import glob
-import yaml
+import functools
+import sys
 
 from . import models
 from . import attributes
 from . import defaults
+from .filecodec import filecodec
 
 from .attributes import GlobalAttributes, VariableAttributes, GroupAttributes
 from .models import Dataset
     
-def get_product(short_name: str) -> Dataset | None:
-    this_dir = os.path.dirname(__file__)
-    products_dir = os.path.join(this_dir, '../products/latest')
+from vocal.utils import get_dataset as vocal_get_dataset
+from vocal.utils import get_spec as vocal_get_spec
 
-    defs = [
-        i for i in glob.glob(os.path.join(products_dir, '*.json'))
-        if not i.endswith('dataset_schema.json')
-    ]
-
-    for d in defs:
-        with open(d, 'r') as y:
-            spec = yaml.load(y, Loader=yaml.Loader)
-            try:
-                if spec['meta']['short_name'] == short_name:
-                    return Dataset.model_validate(spec)
-            except Exception:
-                continue
-
-    return None
-
-def get_spec(short_name: str) -> dict | None:
-    this_dir = os.path.dirname(__file__)
-    products_dir = os.path.join(this_dir, '../products/latest')
-
-    defs = [
-        i for i in glob.glob(os.path.join(products_dir, '*.json'))
-        if not i.endswith('dataset_schema.json')
-    ]
-
-    for d in defs:
-        with open(d, 'r') as y:
-            spec = yaml.load(y, Loader=yaml.Loader)
-            try:
-                if spec['meta']['short_name'] == short_name:
-                    return spec
-            except Exception:
-                continue
-
-    return None
+get_dataset = functools.partial(vocal_get_dataset, project=sys.modules[__name__])
+get_spec = functools.partial(vocal_get_spec, project=sys.modules[__name__])
