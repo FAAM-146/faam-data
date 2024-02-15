fb28b404585207b0817b9f00ce4a38447be04fa0
Dave Sproson <dave.sproson@faam.ac.uk>
2024-02-15 18:43:34 +0000

    Undo accidental renaming of a thing.

--- faam_data/__init__.py
+++ faam_data/__init__.py
@@ -9,8 +9,8 @@ from .filecodec import filecodec
 from .attributes import GlobalAttributes, VariableAttributes, GroupAttributes
 from .models import Dataset
     
-from vocal.utils import get_dataset as vocal_get_dataset
+from vocal.utils import get_product as vocal_get_product
 from vocal.utils import get_spec as vocal_get_spec
 
-get_dataset = functools.partial(vocal_get_dataset, project=sys.modules[__name__])
+get_product = functools.partial(vocal_get_product, project=sys.modules[__name__])
 get_spec = functools.partial(vocal_get_spec, project=sys.modules[__name__])
