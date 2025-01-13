872c31e70f4ebc27dc92dc5ffff46916fc2d3baa
Dave Sproson <dave.sproson@ncas.ac.uk>
2025-01-13 23:47:24 +0000

    Add validator for frequency variable attribute.

--- faam_data/models/variable.py
+++ faam_data/models/variable.py
@@ -1,5 +1,4 @@
-
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 from typing import List
 
 from vocal.field import Field
@@ -8,12 +7,23 @@ from ..attributes import VariableAttributes
 
 
 class VariableMeta(BaseModel):
-    datatype: str = Field(description='The type of the data')
+    datatype: str = Field(description="The type of the data")
     name: str
     required: bool = True
 
 
-class Variable(BaseModel, VocalVariableMixin):       
+class Variable(BaseModel, VocalVariableMixin):
     meta: VariableMeta
     dimensions: List[str]
     attributes: VariableAttributes
+
+    @model_validator(mode="after")
+    @classmethod
+    def validate_frequency(cls, v):
+        frequency = v.attributes.frequency
+        has_sps_dim = any([d.startswith("sps") for d in v.dimensions])
+        if frequency is None and has_sps_dim:
+            raise ValueError(
+                '"frequency" attribute is required when using sps dimension'
+            )
+        return v
